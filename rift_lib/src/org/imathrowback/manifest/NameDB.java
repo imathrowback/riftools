df4b0f21a8f89a12f713b283cb8103253cb571b8
imathrowback <imathrowback@nowhere.com>
2017-09-23 13:01:39 +1000

    Fix to use local entries if found

--- rift_lib/src/org/imathrowback/manifest/NameDB.java
+++ rift_lib/src/org/imathrowback/manifest/NameDB.java
@@ -1,36 +1,53 @@
 package org.imathrowback.manifest;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.function.Function;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.io.IOUtils;
 
-import rift_extractor.assets.ManifestEntry;
-
 public class NameDB
 {
 	static Map<String, String> hashNameMap;
+	static String filename = "single-entries.dat";
 
 	synchronized public static String getNameForHash(final String hash)
+	{
+		return getNameForHash(hash, hash);
+	}
+
+	synchronized public static String getNameForHash(final String hash, final String def)
 	{
 		if (hashNameMap == null)
 		{
 			hashNameMap = new TreeMap<>();
-			Function<ManifestEntry, String> hname = (n) -> hashNameMap.getOrDefault(n.filenameHashStr, "");
 			try
 			{
-				GZIPInputStream singlezip = new GZIPInputStream(
-						NameDB.class.getResourceAsStream("single-entries.dat"));
-				BufferedReader reader = new BufferedReader(new InputStreamReader(singlezip));
-				for (String line : IOUtils.readLines(reader))
+				InputStream is = null;
+				if (new File(filename).exists())
+					is = new FileInputStream(filename);
+				else
+					is = NameDB.class.getResourceAsStream(filename);
+				try
 				{
-					String[] name = line.split(":");
-					if (name.length == 2)
-						hashNameMap.put(name[0], name[1]);
+
+					GZIPInputStream singlezip = new GZIPInputStream(is);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(singlezip));
+					for (String line : IOUtils.readLines(reader))
+					{
+						String[] name = line.split(":");
+						if (name.length == 2)
+							hashNameMap.put(name[0], name[1]);
+					}
+				} finally
+				{
+					if (is != null)
+						is.close();
 				}
 			} catch (Exception ex)
 			{
@@ -39,6 +56,6 @@ public class NameDB
 				return hash;
 			}
 		}
-		return hashNameMap.getOrDefault(hash, hash);
+		return hashNameMap.getOrDefault(hash, def);
 	}
 }
