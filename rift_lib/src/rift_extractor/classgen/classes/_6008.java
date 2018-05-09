08afd75d4af11d5c77d865f57ac1781fc3785aef
imathrowback <imathrowback@nowhere.com>
2018-05-09 10:50:27 +1000

    Formatting

--- rift_lib/src/rift_extractor/classgen/classes/_6008.java
+++ rift_lib/src/rift_extractor/classgen/classes/_6008.java
@@ -1,21 +1,24 @@
 package rift_extractor.classgen.classes;
+
 import org.imathrowback.datparser.CObject;
-import static rift_extractor.classgen.ClassUtils.*;
 import rift_extractor.classgen.ClassUtils;
 
 /** 6008 **/
 @com.thoughtworks.xstream.annotations.XStreamAlias("_6008")
-public class _6008 
+public class _6008
 {
-	public _6008(){}
-	java.lang.String unk0;
-	java.lang.String unk1;
+	public _6008()
+	{
+	}
 
-	public void parse(CObject obj)
+	public java.lang.String unk0;
+	public java.lang.String unk1;
+
+	public void parse(final CObject obj)
 	{
 		ClassUtils.assertType(obj, 6008);
 
-		unk0 = ClassUtils.getFieldMember(java.lang.String.class,obj, 0);
-		unk1 = ClassUtils.getFieldMember(java.lang.String.class,obj, 1);
+		unk0 = ClassUtils.getFieldMember(java.lang.String.class, obj, 0);
+		unk1 = ClassUtils.getFieldMember(java.lang.String.class, obj, 1);
 	}
 }
