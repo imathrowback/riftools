80763f42c84d10b35baeea4e9dbfbf3f48f35ddc
imathrowback <imathrowback@nowhere.com>
2018-05-09 10:50:40 +1000

    Updates names of fields

--- rift_lib/src/rift_extractor/classgen/classes/_7628.java
+++ rift_lib/src/rift_extractor/classgen/classes/_7628.java
@@ -1,35 +1,38 @@
 package rift_extractor.classgen.classes;
+
 import org.imathrowback.datparser.CObject;
-import static rift_extractor.classgen.ClassUtils.*;
 import rift_extractor.classgen.ClassUtils;
 
 /** 7628 **/
 @com.thoughtworks.xstream.annotations.XStreamAlias("_7628")
-public class _7628 
+public class _7628
 {
-	public _7628(){}
-	java.lang.String unk0;
-	TextEntry unk1;
-	java.lang.Long unk2;
+	public _7628()
+	{
+	}
+
+	java.lang.String internalName;
+	public TextEntry textName;
+	java.lang.Long renderableTemplate;
 	java.lang.Long unk3;
-	java.lang.Long unk4;
+	public java.lang.Long iconID_6009_6008;
 	java.util.List<java.lang.Long> unk5;
 	java.lang.Long unk6;
 	java.lang.Boolean unk7;
 	java.lang.Float unk8;
 
-	public void parse(CObject obj)
+	public void parse(final CObject obj)
 	{
 		ClassUtils.assertType(obj, 7628);
 
-		unk0 = ClassUtils.getFieldMember(java.lang.String.class,obj, 0);
-		unk1 = ClassUtils.getFieldMember(TextEntry.class,obj, 1);
-		unk2 = ClassUtils.getFieldMember(java.lang.Long.class,obj, 2);
-		unk3 = ClassUtils.getFieldMember(java.lang.Long.class,obj, 3);
-		unk4 = ClassUtils.getFieldMember(java.lang.Long.class,obj, 4);
-		unk5 = ClassUtils.list(java.lang.Long.class,obj,5);
-		unk6 = ClassUtils.getFieldMember(java.lang.Long.class,obj, 6);
-		unk7 = ClassUtils.getFieldMember(java.lang.Boolean.class,obj, 7);
-		unk8 = ClassUtils.getFieldMember(java.lang.Float.class,obj, 8);
+		internalName = ClassUtils.getFieldMember(java.lang.String.class, obj, 0);
+		textName = ClassUtils.getFieldMember(TextEntry.class, obj, 1);
+		renderableTemplate = ClassUtils.getFieldMember(java.lang.Long.class, obj, 2);
+		unk3 = ClassUtils.getFieldMember(java.lang.Long.class, obj, 3);
+		iconID_6009_6008 = ClassUtils.getFieldMember(java.lang.Long.class, obj, 4);
+		unk5 = ClassUtils.list(java.lang.Long.class, obj, 5);
+		unk6 = ClassUtils.getFieldMember(java.lang.Long.class, obj, 6);
+		unk7 = ClassUtils.getFieldMember(java.lang.Boolean.class, obj, 7);
+		unk8 = ClassUtils.getFieldMember(java.lang.Float.class, obj, 8);
 	}
 }
